ff1273d390881a79a379184f05368563331d2f34
Kaustubh Pimparkar <kaustubhpimparkar123@gmail.com>
2021-08-05 01:31:59 -0700

    commited the fix

--- src/App.js
+++ src/App.js
@@ -41,11 +41,11 @@ class App extends Component {
     return (
       <div className="App">
         <Header data={this.state.resumeData.main}/>
-        {/* <About data={this.state.resumeData.main}/>
+        <About data={this.state.resumeData.main}/>
         <Resume data={this.state.resumeData.resume}/>
         <Portfolio data={this.state.resumeData.portfolio}/>
         <Contact data={this.state.resumeData.main} repos={this.state.resumeData.portfolio}/>
-        <Footer data={this.state.resumeData.main}/> */}
+        <Footer data={this.state.resumeData.main}/>
       </div>
     );
   }
