87cdfee3c1499e68a33fddc2d7685c6ae4e5c4fe
Kaustubh Pimparkar <kaustubhpimparkar123@gmail.com>
2021-08-05 20:45:59 -0700

    Updates

---
+++ precache-manifest.eefc319b823a35b764c754c61a5804dc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "80007a5f8aad8b65ede2",
+    "url": "/portfolio/static/css/main.07318036.chunk.css"
+  },
+  {
+    "revision": "80007a5f8aad8b65ede2",
+    "url": "/portfolio/static/js/main.40f5d75b.chunk.js"
+  },
+  {
+    "revision": "53ebc025fca807d4dbea",
+    "url": "/portfolio/static/js/runtime~main.f308c8e6.js"
+  },
+  {
+    "revision": "791b3f288fcceb99c6bd",
+    "url": "/portfolio/static/js/2.c8a557df.chunk.js"
+  },
+  {
+    "revision": "20e4e3fef5b1a2c09a85d34f3ab708ea",
+    "url": "/portfolio/index.html"
+  }
+];
