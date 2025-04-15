8cfbcde4a0da1a6ed6d1181a4813f73e6bddf7c2
Kaustubh Pimparkar <kaustubh@Kaustubhs-MacBook-Pro.local>
2025-04-15 16:58:14 -0400

    fixed project path

--- src/Components/About.js
+++ src/Components/About.js
@@ -14,6 +14,7 @@ class About extends Component {
       var phone= this.props.data.phone;
       var email = this.props.data.email;
       // var resumeDownload = this.props.data.resumedownload;
+      console.log(this.props.data)
     }
 
     return (
--- src/Components/Portfolio.js
+++ src/Components/Portfolio.js
@@ -5,13 +5,13 @@ class Portfolio extends Component {
 
     if (this.props.data) {
       var projects = this.props.data.projects.map(function (projects) {
-        var projectImage = 'images/portfolio/' + projects.image;
+        // var projectImage = 'images/portfolio/' + projects.image;
         
         return (
           <div key={projects.title} className="columns portfolio-item">
             <div className="item-wrap">
               <a href={projects.url} target="_blank" title={projects.title}>
-                <img alt={projects.title} src={projectImage} />
+                {/* <img alt={projects.title} src={projectImage} /> */}
                 <div className="overlay">
                   <div className="portfolio-item-meta">
                     <h5>{projects.title}</h5>
@@ -33,9 +33,9 @@ class Portfolio extends Component {
           <div className="twelve columns collapsed">
             <h1 style = {{color: "grey", fontSize: "35px", textDecoration: "underline"}}>PROJECTS</h1>
 
-            {/* <div id="portfolio-wrapper" className="bgrid-quarters s-bgrid-thirds cf">
+            <div id="portfolio-wrapper" className="bgrid-quarters s-bgrid-thirds cf">
               {projects}
-            </div> */}
+            </div>
           </div>
         </div>
       </section>
